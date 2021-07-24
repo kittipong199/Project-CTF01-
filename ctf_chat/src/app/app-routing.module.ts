d84a74807824a0aa9c75cb08270b01e7233c36cb
kittipong199 <68522978+kittipong199@users.noreply.github.com>
2021-07-24 23:00:28 +0700

    version1.1 ติดปัญหา Routing หน้า login

--- ctf_chat/src/app/app-routing.module.ts
+++ ctf_chat/src/app/app-routing.module.ts
@@ -1,34 +1,46 @@
-import { NgModule } from '@angular/core';
+/*import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-
-
-
-import { AppComponent } from './app.component';
+import {  FormsModule } from '@angular/forms';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './containers/chat/chat.component';
 
 
 const routes: Routes =[
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'root'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path:'chat',
+    component: ChatComponent
   }
 ];
 
 @NgModule({
   declarations: [
-    AppComponent,
-   
+    
     RegisterComponent,
     LoginComponent
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    CommonModule,
    
     RouterModule.forRoot(routes)
   ],
   exports:[RouterModule]
-})
+})*/
 export class AppRoutingModule { }
--- ctf_chat/src/app/app.module.ts
+++ ctf_chat/src/app/app.module.ts
@@ -1,44 +1,60 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatSliderModule } from '@angular/material/slider';
-
+import { CommonModule } from '@angular/common';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
+import { BackendService } from './backend.service';
+import {  FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-
-import { BackendService } from './backend.service';
-import { RouterModule, Routes } from '@angular/router';
-import {  FormsModule } from '@angular/forms';
 import { ChatComponent } from './containers/chat/chat.component';
-import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+
 
 const routes: Routes =[
   {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home'
+  },
+  {
+    path:'login',
+    component: LoginComponent
+  },
+  {
     path: 'register',
     component: RegisterComponent
+  },
+  {
+    path:'chat',
+    component: ChatComponent
   }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-   
     RegisterComponent,
     LoginComponent,
     ChatComponent,
-    UserComponent
+    HomeComponent
+    
   ],
   imports: [
     BrowserModule,
     FormsModule,
     MatSliderModule,
+    CommonModule,
     RouterModule.forRoot(routes)
+    
   ],
-  providers: [BackendService],
-  bootstrap: [AppComponent]
+  
+  bootstrap: [AppComponent],
+ // exports:[RouterModule],
+  providers: [BackendService]
 })
 export class AppModule { }
